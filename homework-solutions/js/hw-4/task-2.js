fd2ea0f805b646ba64d37593cd40750ffa40bed1
jitelj22 <zamkovec.evgenii@gmail.com>
2025-07-30 21:31:52 +0200

    add fix to task-2

--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -20,12 +20,12 @@ let age = 61;
 
 if (typeof age !== 'number') {
     console.log("Incorrect data type");
-}else if(age < minAge){
+}else if (age < minAge){
     console.log("You don't have access cause your age is " + age + " It's less then " + minAge);
-}else if (age >= minAge && age < maxAge) 
+}else if (age >= minAge && age < maxAge){ 
     console.log("Welcome !");
-else if (age > maxAge) {
+}else if (age > maxAge){
     console.log("Keep calm and look Culture channel.");
 }else {
     console.log("Technical work.");
-} 
+}
